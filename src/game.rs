b6fdc9806ec598a345d9952de9da6b65cf7bda37
Jarno N. Alanko <alanko.jarno@gmail.com>
2023-03-28 01:24:09 +0300

    Print more

--- src/game.rs
+++ src/game.rs
@@ -18,12 +18,21 @@ impl Game{
         let hand = &self.current_hand;
         let street = hand.streets.last().unwrap();
         street.get_available_actions();
+        let (btn_added_chips,bb_added_chips,minimum_raise_size, active_player) = street.get_street_status();
         
         let A = format!("Pot, BB, BTN: {}, {}, {}", hand.pot, hand.bb_stack, hand.btn_stack);
         let B = format!("Button has: {} {}", hand.btn_hole_cards.0.to_string(), hand.btn_hole_cards.1.to_string());
         let C = format!("BB has: {} {}", hand.bb_hole_cards.0.to_string(), hand.bb_hole_cards.1.to_string());
-        let D = format!("{:?}", street.get_available_actions());
-        let state = format!("{}\n{}\n{}\n{}\n", A, B, C, D);
+        let D = format!("Street status (btn added, bb added, to act): {} {} {:?}", btn_added_chips, bb_added_chips, active_player);
+        let E = format!("{:?}", street.get_available_actions());
+
+        let mut board_string = String::from("Board:");
+        for card in &hand.board_cards{
+            let card_str = format!(" {}", card.to_string());
+            board_string.push_str(&card_str);
+        }
+
+        let state = format!("{}\n{}\n{}\n{}\n{}\n{}\n", A, B, C, D, E, board_string);
 
         // TODO: don't send the opponent's hole cards
 
